ec351df4ea6efebc9c5e658011b65c3cb263d15a
yalcinkadir <101977911+yalcinkadir@users.noreply.github.com>
2023-09-06 12:23:02 +0200

    added Placeholder. Sytle removed (cause overwrite)

--- src/views/BookView.vue
+++ src/views/BookView.vue
@@ -7,11 +7,11 @@
     </b-row>
     <b-form @submit.prevent="submitForm">
       <b-form-group label="Check-in Datum:" label-for="checkIn">
-        <b-form-input type="date" id="checkIn" v-model="checkIn" :min="minDate" required></b-form-input>
+        <b-form-input type="date" id="checkIn" v-model="checkIn" required></b-form-input>
       </b-form-group>
 
       <b-form-group label="Check-out Datum:" label-for="checkOut">
-        <b-form-input type="date" id="checkOut" v-model="checkOut" :min="minDate" required></b-form-input>
+        <b-form-input type="date" id="checkOut" v-model="checkOut" required></b-form-input>
       </b-form-group>
 
       <b-form-group label="Anzahl der Personen:" label-for="persons">
@@ -19,7 +19,7 @@
       </b-form-group>
 
       <b-form-group label="Zimmerauswahl:" label-for="room">
-        <b-form-select id="room" v-model="selectedRoom" :options="roomOptions" @change="updateRoomExtras"></b-form-select>
+        <b-form-select id="room" v-model="selectedRoom" :options="roomOptions" :first="firstOption" @change="updateRoomExtras"></b-form-select>
       </b-form-group>
 
       <div v-if="selectedRoomExtras.length > 0">
@@ -39,30 +39,33 @@ import { ref, onMounted, computed } from 'vue';
 import axios from 'axios';
 
 // Initialize ref variables
-const minDate = ref(new Date().toISOString().split('T')[0]);  // Current Date
+const firstOption = ref({ text: 'Wählen Sie ein Zimmer', value: null }); // New variable
 const checkIn = ref(new Date().toISOString().split('T')[0]);  // Current Date
 const checkOut = ref(new Date(new Date().getTime() + 24 * 60 * 60 * 1000).toISOString().split('T')[0]); // Next Day
 const persons = ref(1);
 const rooms = ref([]);
-const selectedRoom = ref(null);
+const selectedRoom = ref(null); // Change this to initially be null
 const selectedRoomExtras = ref([]);
+
+const roomOptions = computed(() => {
+  return [
+    { text: 'Wählen Sie ein Zimmer', value: null },
+    ...rooms.value.map(room => ({
+      value: room.id,
+      text: `${room.roomsName} - ${room.pricePerNight} € pro Nacht`,
+    }))
+  ];
+});
 
 async function fetchRooms() {
   try {
     const response = await axios.get('https://boutique-hotel.helmuth-lammer.at/api/v1/rooms');
     rooms.value = response.data;
-    selectedRoom.value = rooms.value[0]?.id;
+    selectedRoom.value = null;  // Setzen Sie dies auf null
   } catch (error) {
     console.error('Fehler beim Abrufen der Zimmerdaten:', error);
   }
 }
-
-const roomOptions = computed(() => {
-  return rooms.value.map(room => ({
-    value: room.id,
-    text: `${room.roomsName} - ${room.pricePerNight} € pro Nacht`,
-  }));
-});
 
 function updateRoomExtras() {
   const selected = rooms.value.find(room => room.id === selectedRoom.value);
@@ -94,10 +97,3 @@ onMounted(() => {
   updateRoomExtras();
 });
 </script>
-
-<style>
-/* Benutzerdefinierte CSS-Regel, um die Schriftfarbe der ausgewählten Option zu ändern */
-.select-selected {
-  color: rgba(0, 0, 0, 0.5);
-}
-</style>
