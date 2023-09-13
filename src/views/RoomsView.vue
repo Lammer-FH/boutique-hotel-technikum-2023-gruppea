3eb0055a8d2796bfbc0d85407b1af42b528db353
yalcinkadir <101977911+yalcinkadir@users.noreply.github.com>
2023-09-13 23:43:50 +0200

    shown photos of the first 5 rooms

--- src/views/RoomsView.vue
+++ src/views/RoomsView.vue
@@ -1,16 +1,46 @@
 <template>
-    <div class="rooms">
-      <h1>Our Rooms</h1>
+  <div class="rooms">
+    <h1>Our Rooms</h1>
+
+    <div class="masonry">
+      <div v-for="room in rooms.slice(0, 5)" :key="room.id">
+        <img :src="`src/images/rooms/${room.roomsNumber}.jpg`" :alt="room.roomsNumber" :title="`${room.roomsName} - Room (${room.roomsNumber})`">
+      </div>
     </div>
-  </template>
-  
-  <style>
-  @media (min-width: 1024px) {
-    .rooms {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      rooms: []
+    }
+  },
+  async created() {
+    try {
+      const response = await axios.get('https://boutique-hotel.helmuth-lammer.at/api/v1/rooms');
+      this.rooms = response.data;
+    } catch (error) {
+      console.error("Error fetching rooms:", error);
     }
   }
-  </style>
-  
+}
+</script>
+
+<style>
+.masonry {
+  display: grid;
+  grid-gap: 16px;
+  grid-template-columns: repeat(auto-fill, minmax(15%, 1fr));
+  grid-auto-rows: 100%;
+}
+
+.masonry img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
