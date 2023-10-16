7a0f32e60987b57ea99648bff789b9955198b414
yalcinkadir <101977911+yalcinkadir@users.noreply.github.com>
2023-10-16 19:50:17 +0200

    adapted icons in RoomsView from RoomExtrasIcons

--- src/components/RoomExtrasIcons.vue
+++ src/components/RoomExtrasIcons.vue
@@ -19,52 +19,82 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import { ref } from 'vue';
 
 export default {
-  props: {
-    roomExtras: {
-      type: Array,
-      default: () => [],
-    },
-  },
-  setup(props) {
-    const roomExtras = ref(props.roomExtras);
-
-    const EXTRAS = {
-      BATHROOM: "bathroom",
-      MINIBAR: "minibar",
-      TELEVISION: "television",
-      LIVINGROOM: "livingroom",
-      AIRCONDITION: "aircondition",
-      WIFI: "wifi",
-      BREAKFAST: "breakfast",
-      HANDICAPPED_ACCESSIBLE: "handicapped accessible",
-    };
-
-    const extraToIcon = (extraName) => {
-        console.log('!!!!!!!!!!!! ExtraName', extraName);
-      const mapping = {
-        [EXTRAS.BATHROOM]: { library: "fa", icon: "fa fa-bath" },
-        [EXTRAS.MINIBAR]: { library: "fa", icon: "fa-solid fa-wine-glass" },
-        [EXTRAS.TELEVISION]: { library: "bi", icon: "bi-tv" },
-        [EXTRAS.LIVINGROOM]: { library: "fa", icon: "fa-solid fa-couch" },
-        [EXTRAS.AIRCONDITION]: { library: "fa", icon: "fa-solid fa-fan" },
-        [EXTRAS.WIFI]: { library: "bi", icon: "bi-wifi" },
-        [EXTRAS.BREAKFAST]: { library: "fa", icon: "fa-solid fa-mug-saucer" },
-        [EXTRAS.HANDICAPPED_ACCESSIBLE]: {
-          library: "bi",
-          icon: "bi-person-wheelchair",
+    props: {
+        roomExtras: {
+            type: Array,
+            default: () => [],
         },
-      };
-      const key = Object.keys(extraName)[0];
-      return mapping[EXTRAS[key]] || { library: "bi", icon: "bi-question" };
-    };
+    },
+    setup(props) {
+        const roomExtras = ref(props.roomExtras);
 
-    return {
-      roomExtras,
-      extraToIcon,
-    };
-  },
+        const EXTRAS = {
+            BATHROOM: "bathroom",
+            MINIBAR: "minibar",
+            TELEVISION: "television",
+            LIVINGROOM: "livingroom",
+            AIRCONDITION: "aircondition",
+            WIFI: "wifi",
+            BREAKFAST: "breakfast",
+            HANDICAPPED_ACCESSIBLE: "handicapped_accessible",
+        };
+
+        const extraToIcon = (extraName) => {
+            const mapping = {
+                [EXTRAS.BATHROOM]: { library: "fa", icon: "fa fa-bath" },
+                [EXTRAS.MINIBAR]: { library: "fa", icon: "fa-solid fa-wine-glass" },
+                [EXTRAS.TELEVISION]: { library: "bi", icon: "bi-tv" },
+                [EXTRAS.LIVINGROOM]: { library: "fa", icon: "fa-solid fa-couch" },
+                [EXTRAS.AIRCONDITION]: { library: "fa", icon: "fa-solid fa-fan" },
+                [EXTRAS.WIFI]: { library: "bi", icon: "bi-wifi" },
+                [EXTRAS.BREAKFAST]: { library: "fa", icon: "fa-solid fa-mug-saucer" },
+                [EXTRAS.HANDICAPPED_ACCESSIBLE]: {
+                    library: "bi",
+                    icon: "bi-person-wheelchair",
+                },
+            };
+            const key = Object.keys(extraName)[0];
+            const keyUpperCase = key.toUpperCase();
+            const formattedKey = key.toUpperCase().replace(' ', '_');
+            
+            console.log('EXTRAS[key]:', EXTRAS[key]);
+            console.log('key:', key);
+            return mapping[EXTRAS[formattedKey]] || { library: "bi", icon: "bi-question" };
+            // return mapping[EXTRAS[keyUpperCase]] || { library: "bi", icon: "bi-question" };
+
+        };
+
+        return {
+            roomExtras,
+            extraToIcon,
+        };
+    },
 };
 </script>
+
+<style scoped>
+.room-extras {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    flex-grow: 1;
+}
+
+.icon-button {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    font-size: 1.8rem;
+    color: #0065fcae;
+    transition: color 0.3s;
+}
+
+.icon-button:hover {
+    color: #0563f9;
+}
+</style>
 
 
 
