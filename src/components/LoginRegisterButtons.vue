ad7a6f9615bbb0fe8b342e7b6a2b7ac96ebd815b
yalcinkadir <101977911+yalcinkadir@users.noreply.github.com>
2023-11-04 19:27:19 +0100

    Refactor login and register buttons to use a single modal

--- src/components/LoginRegisterButtons.vue
+++ src/components/LoginRegisterButtons.vue
@@ -1,19 +1,30 @@
+<!-- LoginRegisterButtons.vue -->
 <template>
     <div class="logRegButton">
-        <button @click="toggleLoginModal" class="btn btn-outline-secondary">Anmelden</button>
-        <button @click="toggleRegisterModal" class="btn btn-success">Registrieren</button>
+        <button @click="toggleModal(true)" class="btn btn-outline-secondary">Anmelden</button>
+        <button @click="toggleModal(false)" class="btn btn-success">Registrieren</button>
+        <!-- Das Test-Popup -->
+        <div v-if="showModal" class="modal">
+            <div class="modal-content">
+                <span class="close" @click="showModal = false">&times;</span>
+                <p>{{ isLogin ? 'Anmeldeformular' : 'Registrierungsformular' }}</p>
+            </div>
+        </div>
     </div>
 </template>
   
 <script setup>
-import { ref } from 'vue';
+import { ref, defineEmits } from 'vue';
 
-const toggleLoginModal = () => {
-    // Dein Code zum Öffnen des Login-Modals
-};
+const showModal = ref(false);
+const isLogin = ref(true);
+const emit = defineEmits(['update:showModal', 'update:isLogin']);
 
-const toggleRegisterModal = () => {
-    // Dein Code zum Öffnen des Register-Modals
+const toggleModal = (login) => {
+    showModal.value = true;
+    isLogin.value = login;
+    emit('update:showModal', showModal.value);
+    emit('update:isLogin', isLogin.value);
 };
 </script>
   
@@ -23,17 +34,64 @@ const toggleRegisterModal = () => {
     align-items: center;
     justify-content: space-between;
 }
+
+/* ... dein bestehender Stil ... */
+/* Stil für das Test-Popup */
+.modal {
+    display: block;
+    /* Hidden by default */
+    position: fixed;
+    /* Stay in place */
+    z-index: 1;
+    /* Sit on top */
+    left: 0;
+    top: 0;
+    width: 100%;
+    /* Full width */
+    height: 100%;
+    /* Full height */
+    overflow: auto;
+    /* Enable scroll if needed */
+    background-color: rgb(0, 0, 0);
+    /* Fallback color */
+    background-color: rgba(0, 0, 0, 0.4);
+    /* Black w/ opacity */
+}
+
+.modal-content {
+    background-color: #fefefe;
+    margin: 15% auto;
+    /* 15% from the top and centered */
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%;
+    /* Could be more or less, depending on screen size */
+}
+
+.close {
+    color: #aaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+}
+
 button.btn.btn-outline-secondary {
     border: transparent;
     font-weight: 600;
     --bs-btn-hover-bg: none;
     --bs-btn-hover-color: #1dbf73;
 }
+
 button.btn.btn-success {
     --bs-btn-color: #1dbf73;
     --bs-btn-hover-bg: #1dbf73;
     --bs-btn-bg: none;
     border-color: #1dbf73;
-}
-</style>
-  
+}</style>
--- src/views/popups/LoginAndRegister.vue
+++ src/views/popups/LoginAndRegister.vue
@@ -1,57 +1,38 @@
 <!-- LoginAndRegister.vue -->
 <template>
   <div>
-    <!-- Buttons zum Öffnen der Modals -->
-    <button @click="showLoginModal" class="btn btn-outline-secondary">Anmelden</button>
-    <button @click="showRegisterModal" class="btn btn-success">Registrieren</button>
-
-    <!-- Modal für die Anmeldung -->
-    <b-modal id="login-modal" title="Anmelden">
-      <b-form @submit.prevent="submitLoginForm">
-        <b-form-group label="Benutzername" class="mb-3">
-          <b-form-input v-model="loginUsername" required></b-form-input>
-        </b-form-group>
-        <b-form-group label="Passwort" class="mb-3">
-          <b-form-input type="password" v-model="loginPassword" required></b-form-input>
-        </b-form-group>
-        <b-button type="submit" variant="primary">Anmelden</b-button>
-      </b-form>
-    </b-modal>
-
-    <!-- Modal für die Registrierung -->
-    <b-modal id="register-modal" title="Registrieren">
-      <b-form @submit.prevent="submitRegisterForm">
-        <b-form-group label="Benutzername" class="mb-3">
-          <b-form-input v-model="registerUsername" required></b-form-input>
-        </b-form-group>
-        <b-form-group label="Passwort" class="mb-3">
-          <b-form-input type="password" v-model="registerPassword" required></b-form-input>
-        </b-form-group>
-        <b-button type="submit" variant="primary">Registrieren</b-button>
-      </b-form>
-    </b-modal>
+      <!-- Das Test-Popup -->
+      <div v-if="showModal" class="modal">
+          <div class="modal-content">
+              <span class="close" @click="showModal = false">&times;</span>
+              <p>{{ isLogin ? 'Anmeldeformular' : 'Registrierungsformular' }}</p>
+          </div>
+      </div>
   </div>
 </template>
-
 <script setup>
-import { ref } from 'vue';
+import { ref, watch, defineProps } from 'vue';
 
-const showLoginModal = ref(false);
-const showRegisterModal = ref(false);
+// Definiere die Props
+const props = defineProps({
+  showModalProp: Boolean,
+  isLoginProp: Boolean
+});
 
-// Daten für die Anmelde- und Registrierungsformulare
-const loginUsername = ref('');
-const loginPassword = ref('');
-const registerUsername = ref('');
-const registerPassword = ref('');
+// Reaktive Referenzen, die mit den Props initialisiert werden
+const showModal = ref(props.showModalProp);
+const isLogin = ref(props.isLoginProp);
 
-// Dummy-Funktionen für die Formulareinreichung
-const submitLoginForm = () => {
-  console.log('Anmeldeformular eingereicht:', { username: loginUsername.value, password: loginPassword.value });
-};
+// Beobachte die Props für Änderungen und aktualisiere die lokalen Referenzen
+watch(props, (newProps) => {
+  showModal.value = newProps.showModalProp;
+  isLogin.value = newProps.isLoginProp;
+});
 
-const submitRegisterForm = () => {
-  console.log('Registrierungsformular eingereicht:', { username: registerUsername.value, password: registerPassword.value });
+// Eine Methode, um das Modal zu schließen
+const closeModal = () => {
+  showModal.value = false;
+  // Hier könntest du auch ein Ereignis emittieren, um die übergeordnete Komponente zu informieren
 };
 </script>
 
