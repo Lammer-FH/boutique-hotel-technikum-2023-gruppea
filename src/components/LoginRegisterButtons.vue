753018637f53d6b4c879c4f423f5ad8f8171cb7c
yalcinkadir <101977911+yalcinkadir@users.noreply.github.com>
2023-11-04 22:30:05 +0100

    added registration to API

--- src/components/LoginRegisterButtons.vue
+++ src/components/LoginRegisterButtons.vue
@@ -1,33 +1,154 @@
 <!-- LoginRegisterButtons.vue -->
 <template>
     <div class="logRegButton">
-        <button @click="toggleModal(true)" class="btn btn-outline-secondary">Anmelden</button>
-        <button @click="toggleModal(false)" class="btn btn-success">Registrieren</button>
-        <!-- Das Test-Popup -->
-        <div v-if="showModal" class="modal">
-            <div class="modal-content">
-                <span class="close" @click="showModal = false">&times;</span>
-                <p>{{ isLogin ? 'Anmeldeformular' : 'Registrierungsformular' }}</p>
-            </div>
-        </div>
+        <!-- Buttons zum Öffnen der Modals -->
+        <button @click="toggleModal('login')" class="btn btn-outline-secondary">Anmelden</button>
+        <button @click="toggleModal('register')" class="btn btn-success">Registrieren</button>
+
+        <!-- Einzelnes Modal für die Anmeldung und Registrierung -->
+        <b-modal v-model="showModal" :title="modalState === 'login' ? 'Anmelden' : 'Registrieren'">
+            <b-form @submit.prevent="modalState === 'login' ? submitLoginForm() : submitRegisterForm()">
+
+                <!-- E-Mail Feld für beide Formulare -->
+                <b-form-group label="E-Mail" class="mb-3" describedby="email-help">
+                    <b-form-input type="email" v-model="email" autocomplete="username" placeholder="beispiel@domain.com"
+                        aria-describedby="email-help" @blur="emailBlurred = true"
+                        :state="email && emailBlurred ? validateEmail(email) : null" required></b-form-input>
+                    <b-form-invalid-feedback v-if="emailBlurred && !validateEmail(email)">
+                        Bitte gib eine gültige E-Mail-Adresse ein.
+                    </b-form-invalid-feedback>
+                    <b-form-text id="email-help">Deine E-Mail für die Anmeldung.</b-form-text>
+                </b-form-group>
+
+                <!-- Registrierungsfelder -->
+                <template v-if="modalState === 'register'">
+                    <b-form-group label="Vorname" class="mb-3">
+                        <b-form-input v-model="firstName" required autocomplete="firstname"></b-form-input>
+                    </b-form-group>
+                    <b-form-group label="Nachname" class="mb-3">
+                        <b-form-input v-model="lastName" required autocomplete="lastname"></b-form-input>
+                    </b-form-group>
+                    <b-form-group label="Geburtstag" class="mb-3">
+                        <b-form-input type="date" v-model="birthday" required autocomplete="birthday"></b-form-input>
+                    </b-form-group>
+                    <b-form-group label="Passwort Bestätigen" class="mb-3">
+                        <b-form-input type="password" v-model="confirmPassword" autocomplete="new-password"
+                            required></b-form-input>
+                    </b-form-group>
+                </template>
+
+                <!-- Passwortfeld für beide Formulare -->
+                <b-form-group label="Passwort" class="mb-3">
+                    <b-form-input type="password" v-model="password"
+                        :autocomplete="modalState === 'login' ? 'current-password' : 'new-password'"
+                        required></b-form-input>
+                </b-form-group>
+
+                <!-- Anmelde- oder Registrierungsknopf -->
+                <b-button type="submit" variant="primary">{{ modalState === 'login' ? 'Anmelden' : 'Registrieren'
+                }}</b-button>
+            </b-form>
+        </b-modal>
     </div>
 </template>
   
 <script setup>
-import { ref, defineEmits } from 'vue';
+import { ref } from 'vue';
+import axios from 'axios';
+import { BModal, BForm, BFormGroup, BFormInput, BButton } from 'bootstrap-vue-3';
 
 const showModal = ref(false);
-const isLogin = ref(true);
-const emit = defineEmits(['update:showModal', 'update:isLogin']);
+const modalState = ref('login'); // 'login' oder 'register'
+// const email = ref('');
+// const firstName = ref('');
+// const lastName = ref('');
+// const password = ref('');
+// const confirmPassword = ref('');
+const emailBlurred = ref(false);
+// const birthday = ref('');
+const registrationSuccess = ref(false);
 
-const toggleModal = (login) => {
-    showModal.value = true;
-    isLogin.value = login;
-    emit('update:showModal', showModal.value);
-    emit('update:isLogin', isLogin.value);
+const apiURL = 'https://boutique-hotel.helmuth-lammer.at/api/v1/register';
+const registerUser = async () => {
+    if (password.value !== confirmPassword.value) {
+        alert('Die Passwörter stimmen nicht überein.');
+        return;
+    }
+
+    try {
+        const response = await axios.post(apiURL, {
+            firstname: firstName.value,
+            lastname: lastName.value,
+            email: email.value,
+            username: email.value,
+            password: password.value
+        });
+
+        // Verarbeite die Antwort des Servers
+        console.log('Registrierung erfolgreich', response.data);
+        // Zeige eine Bestätigungsnachricht an
+        alert('Registrierung erfolgreich!');
+        showModal.value = false; // Schließe das Modal
+
+    } catch (error) {
+        // Fehlerbehandlung
+        console.error('Registrierung fehlgeschlagen', error);
+        // Zeige eine Fehlermeldung an
+        alert('Registrierung fehlgeschlagen: ' + error.response.data.message);
+    }
 };
+
+const toggleModal = (state) => {
+    showModal.value = true;
+    modalState.value = state;
+};
+
+const submitLoginForm = () => {
+    // Logik für die Einreichung des Anmeldeformulars
+    console.log('Anmelden', email.value, password.value);
+    showModal.value = false; // Modal schließen
+};
+
+const submitRegisterForm = async () => {
+    if (password.value !== confirmPassword.value) {
+        alert('Die Passwörter stimmen nicht überein.');
+        return;
+    }
+    await registerUser();
+    // Logik für die Einreichung des Registrierungsformulars
+    console.log('Registrieren', firstName.value, lastName.value, email.value, password.value);
+    showModal.value = false; // Modal schließen
+};
+
+const validateEmail = (email) => {
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return pattern.test(email);
+};
+
+const formatDate = (date) => {
+    let hours = date.getHours().toString().padStart(2, '0');
+    let minutes = date.getMinutes().toString().padStart(2, '0');
+    let seconds = date.getSeconds().toString().padStart(2, '0');
+    return `${date.getDate()}${date.getMonth() + 1}${date.getFullYear()}_${hours}${minutes}${seconds}`;
+};
+
+// Setze die initialen Werte für die Testdaten
+const currentDate = new Date();
+const email = ref(`${formatDate(new Date())}@test.com`);
+const firstName = ref('Max');
+const lastName = ref('Mustermann');
+const password = ref('Passwort123');
+const confirmPassword = ref('Passwort123');
+const birthday = ref(`${currentDate.getFullYear() - 18}-${(currentDate.getMonth() + 1).toString().padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')}`);
 </script>
-  
+
+
+
+
+
+
+
+
 <style scoped>
 .logRegButton {
     display: flex;
@@ -94,4 +215,5 @@ button.btn.btn-success {
     --bs-btn-hover-bg: #1dbf73;
     --bs-btn-bg: none;
     border-color: #1dbf73;
-}</style>
+}
+</style>
